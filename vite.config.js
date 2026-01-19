93b31cbd73e0278153d6058b19d527f99f18713a
Warren Spier <warren.spier@gmail.com>
2026-01-19 13:36:39 +1100

    Implementing Vercel Analytics

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Analytics } from '@vercel/analytics/react'; // This is for Vite/React
 import tailwindcss from '@tailwindcss/vite' // This is the line that defines "tailwindcss"
 
 // https://vitejs.dev/config/
